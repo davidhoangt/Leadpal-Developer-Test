88947c79b4ae44a334884005d4ad873075fd6028
davidhoangt <davidhoangt@gmail.com>
2020-09-03 21:38:19 +1000

    Update index.php
    Finished styling and had to fix errors with API calls. Cannot comment in HTML as text editor does not allow.

--- index.php
+++ index.php
@@ -1,12 +1,10 @@
 <?php
-
+//API Key and OpenWeatherMap APIs to receive data
+//php script retrieves data from OpenWeathermp API and decodes into a JSON for use in HTML code
 $apiKey = "0b26cc5017624800da6d3057f5191af9";
-$cityId ="1273293";
-$url = 'api.openweathermap.org/data/2.5/weather?q=Brisbane&appid=0b26cc5017624800da6d3057f5191af9';
-$url2 = 'pro.openweathermap.org/data/2.5/forecast/hourly?q=Brisbane&appid=0b26cc5017624800da6d3057f5191af9';
+$ch_1 = curl_init('api.openweathermap.org/data/2.5/weather?q=Brisbane&mode=json&units=metric&appid=0b26cc5017624800da6d3057f5191af9');
+$ch_2 = curl_init('api.openweathermap.org/data/2.5/forecast?q=Brisbane&mode=json&units=metric&&appid=0b26cc5017624800da6d3057f5191af9');
 
-$ch_1 = curl_init('api.openweathermap.org/data/2.5/weather?q=Brisbane&appid=0b26cc5017624800da6d3057f5191af9');
-$ch_2 = curl_init('api.openweathermap.org/data/2.5/forecast?q=Brisbane&appid=0b26cc5017624800da6d3057f5191af9');
 curl_setopt($ch_1, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch_2, CURLOPT_RETURNTRANSFER, true);
 
@@ -29,24 +27,21 @@ $response_2 = curl_multi_getcontent($ch_2);
 
 $data = json_decode($response_1);
 $data2 = json_decode($response_2);
-//echo "$response_1"; 
 ?>
-
-
 
 <!DOCTYPE HTML>
 <html>
 <head>
-	<title>Forecast Weather - OpenWeatherMap</title>
+	<title>Brisbane Weather- OpenWeatherMap</title>
 	<meta name='viewport' content='width=device-width, initial-scale=1'>
 	<script src='https://kit.fontawesome.com/a076d05399.js'></script>
+	<link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@500&display=swap" rel="stylesheet">
 </head>
 <style>
-
 .container	{
 	display: flex;
 	justify-content: center;
-	height: 50%;
+	padding: 70px 20px;
 }
 .forecastWeather_container	{
 	display: flex;
@@ -57,126 +52,121 @@ $data2 = json_decode($response_2);
 }
 .currentWeather_container{
 	text-align: center;
-	width: 500px;
-	background-color: rgba(0, 0, 0, 0.4);
+	width: 800px;
+	background-color: rgba(0, 0, 0, 0.6);
 	color: white;
+	height: 500px;
 }
 .nextDay{
-	background-color: rgba(0, 0, 0, 0.4);
+	background-color: rgba(0, 0, 0, 0.6);
 	display: flex;
 	justify-content: space-around;
+	width: 500px;
+	align-items: center;
 }
 .splitContent{
 	float: left;
 	color: white;
-	width: 40%;
-	height: 300px;
-	padding: 10px;
+	margin: 50px;
+	text-align: center;
 }
 #temp{
-	font-size: 50px;
+	font-size: 80px;
 	padding: 0;
 	margin: 0;
 }
 p	{
 	margin: auto;
 }
-.currentWeather_section{
-	height: 50%;
-	padding: 50px 20px;
-}
-
 body{
 	margin: 100px 100px;
 	background-image: url('images/brisbanecity.jpg');
 	background-repeat: no-repeat;
 	background-size: cover; 
 	vertically-align: middle;
+	font-family: 'Roboto Slab', serif;
 }
-
 </style>
 <body>
-		<section class="currentWeather_section">
-			<div class="container">
-				<div class="currentWeather_container"> 
-					<h1><?php echo $data->name; ?></h1>
-					<p id="temp"><i class='fas fa-cloud-sun' style='font-size:100px'></i>
-					<?php echo $data->weather[0]->description; ?>
-					</p><br>
-					<div>
-					<p id="temp"><?php echo $data->main->temp; ?>°</p> 
-					<p>
-					Min <?php echo $data->main->temp_min; ?><br>
-					Max <?php echo $data->main->temp_max; ?></p>
-					</div>
-					<p>Temperature feels like: <?php echo $data->main->feels_like;; ?></p>
-					<p>Sunrise and Sunset times 
-						<?php echo $data->sys->sunrise; ?>
-						<?php echo $data->sys->sunset; ?>
-					</p>
-					<p>Humidity <?php echo $data->main->humidity; ?>%</p>
-				</div>
-			</div>
-		</section>
-		
-		<section>
-	
-		<div class="container">
-			<div class="forecastWeather_container">
-			
-				<div class="nextDay">
-				<div class="splitContent">
-					<i class='fas fa-cloud-sun' style='font-size:36px'></i>
-					<p> General Weather Description <?php echo $data->weather[0]->description; ?></p>
-					<p>Temperature <?php echo $data->main->temp; ?></p>
-					<p>Humidity <?php echo $data->main->humidity; ?></p>
-				</div>
-				
-				<div class="splitContent"> 		
-					<i class='fas fa-cloud-sun' style='font-size:36px'></i>
-					<p> General Weather Description <?php echo $data->weather[0]->description; ?></p>
-					<p>Temperature <?php echo $data->main->temp; ?></p>
-					<p>Humidity <?php echo $data->main->humidity; ?></p>
-				</div>
-				</div>
-			
-				<div class="nextDay">
-				<div class="splitContent">
-					<i class='fas fa-cloud-sun' style='font-size:36px'></i>
-					<p> General Weather Description <?php echo $data->weather[0]->description; ?></p>
-					<p>Temperature <?php echo $data->main->temp; ?></p>
-					<p>Humidity <?php echo $data->main->humidity; ?></p>
-				</div>
-				
-				<div class="splitContent"> 		
-					<i class='fas fa-cloud-sun' style='font-size:36px'></i>
-					<p> General Weather Description <?php echo $data->weather[0]->description; ?></p>
-					<p>Temperature <?php echo $data->main->temp; ?></p>
-					<p>Humidity <?php echo $data->main->humidity; ?></p>
-				</div>
-				</div>
-			
-				<div class="nextDay">
-				<div class="splitContent">
-					<i class='fas fa-cloud-sun' style='font-size:36px'></i>
-					<p> General Weather Description <?php echo $data->weather[0]->description; ?></p>
-					<p>Temperature <?php echo $data->main->temp; ?></p>
-					<p>Humidity <?php echo $data->main->humidity; ?></p>
-				</div>
-				
-				<div class="splitContent"> 		
-					<i class='fas fa-cloud-sun' style='font-size:36px'></i>
-					<p> General Weather Description <?php echo $data->weather[0]->description; ?></p>
-					<p>Temperature <?php echo $data->main->temp; ?></p>
-					<p>Humidity <?php echo $data->main->humidity; ?></p>
-				</div>
-				</div>
 
-		</div>
+	<section>
+		<div class="container">
+			<div class="currentWeather_container"> 
+				<h1 style="font-size: 40px; padding: 0; margin: 10px;"><?php echo $data->name; ?></h1>
+				<p id="temp"><i class='fas fa-cloud-sun' style='font-size:150px'></i></p>
+				<p><?php echo $data->weather[0]->description; ?></p>
+					<div>
+						<p id="temp"><?php echo $data->main->temp; ?>°C</p> 
+						<p>Min <?php echo $data->main->temp_min; ?>
+						<br>Max <?php echo $data->main->temp_max; ?></p>
+					</div>
+				<p>Temperature feels like: <?php echo $data->main->feels_like;; ?></p>
+				<p>Sunrise Time: <?php echo $data->sys->sunrise; ?> </p>
+				<p>Sunset Time: <?php echo $data->sys->sunset; ?></p>
+				<p>Humidity: <?php echo $data->main->humidity; ?>%</p>
+			</div>
 		</div>
 	</section>
 	
-
+	<section>
+		<div class="container">
+			<div class="forecastWeather_container">
+				<div class="nextDay">
+					<div class="splitContent">
+						<h2 style="text-align:left;">Tomorrow</h2>
+						<p>9am</p><br>
+						<i class='fas fa-sun' style='font-size:70px'></i>
+						<p><?php echo $data2->list[7]->weather[0]->description ?></p><br>
+						<p>Temperature: <?php echo $data2->list[7]->main->temp; ?>°C</p>
+						<p>Humidity: <?php echo $data2->list[7]->main->humidity; ?>%</p>
+					</div>
+					<div class="splitContent">
+						<br><br><br><p>3pm</p>
+						<br><i class='fas fa-sun' style='font-size:70px'></i>
+						<p><?php echo $data2->list[9]->weather[0]->description ?></p><br>
+						<p>Temperature: <?php echo $data2->list[9]->main->temp; ?>°C</p>
+						<p>Humidity: <?php echo $data2->list[9]->main->humidity; ?>%</p>
+					</div>
+				</div>
+				<div class="nextDay">
+					<div class="splitContent">
+						<h2 style="text-align:left;">In 2 Days</h2>
+						<p>9am</p><br>
+						<i class='fas fa-cloud-sun' style='font-size:70px'></i>
+						<p><?php echo $data2->list[17]->weather[0]->description ?></p><br>
+						<p>Temperature: <?php echo $data2->list[17]->main->temp; ?>°C</p>
+						<p>Humidity: <?php echo $data2->list[17]->main->humidity; ?>%</p>
+					</div>
+				
+					<div class="splitContent"> 		
+						<br><br><br><p>3pm</p><br>
+						<i class='fas fa-cloud-sun' style='font-size:70px'></i>
+						<p><?php echo $data2->list[19]->weather[0]->description ?></p><br>
+						<p>Temperature: <?php echo $data2->list[19]->main->temp; ?>°C</p>
+						<p>Humidity: <?php echo $data2->list[19]->main->humidity; ?>%</p>
+					</div>
+				</div>
+				<div class="nextDay">
+					<div class="splitContent">
+						<h2 style="text-align:left;">In 3 Days</h2>
+						<p>9am</p><br>
+						<i class='fas fa-cloud-rain' style='font-size:70px'></i>
+						<p><?php echo $data2->list[27]->weather[0]->description ?></p><br>
+						<p>Temperature: <?php echo $data2->list[27]->main->temp; ?>°C</p>
+						<p>Humidity: <?php echo $data2->list[27]->main->humidity; ?>%</p>
+					</div>
+				
+					<div class="splitContent"> 		
+						<br><br><br><p>3pm</p><br>
+						<i class='fas fa-sun' style='font-size:70px'></i>
+						<p><?php echo $data2->list[29]->weather[0]->description ?></p><br>
+						<p>Temperature: <?php echo $data2->list[29]->main->temp; ?>°C</p>
+						<p>Humidity: <?php echo $data2->list[29]->main->humidity; ?>%</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
 </body>
 </html>
 
